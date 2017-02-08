409b8ba2a41de54c0068a24bacbc3d6105830d60
Brighty28 <abright2012@gmail.com>
2017-02-08 16:35:10 +0000

    Lucence updated property results

--- TpayneandCo/App_Data/Models/all.generated.cs
+++ TpayneandCo/App_Data/Models/all.generated.cs
@@ -9,7 +9,7 @@ using  Umbraco.ModelsBuilder;
 using  Umbraco.ModelsBuilder.Umbraco;
 [assembly: PureLiveAssembly]
 [assembly:ModelsBuilderAssembly(PureLive = true, SourceHash = "55111bf6d051410b")]
-[assembly:System.Reflection.AssemblyVersion("0.0.0.2")]
+[assembly:System.Reflection.AssemblyVersion("0.0.0.1")]
 
 
 // FILE: models.generated.cs
