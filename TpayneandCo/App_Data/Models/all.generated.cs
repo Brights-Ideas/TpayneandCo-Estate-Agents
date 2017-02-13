cd88a5aecf9ca2547fec20d0a403902e75f6ddc2
Andrew Bright <abright2012@gmail.com>
2017-02-13 22:16:36 +0000

    Latest

--- TpayneandCo/App_Data/Models/all.generated.cs
+++ TpayneandCo/App_Data/Models/all.generated.cs
@@ -9,7 +9,7 @@ using  Umbraco.ModelsBuilder;
 using  Umbraco.ModelsBuilder.Umbraco;
 [assembly: PureLiveAssembly]
 [assembly:ModelsBuilderAssembly(PureLive = true, SourceHash = "f5f2b42ea76e8f26")]
-[assembly:System.Reflection.AssemblyVersion("0.0.0.2")]
+[assembly:System.Reflection.AssemblyVersion("0.0.0.1")]
 
 
 // FILE: models.generated.cs
